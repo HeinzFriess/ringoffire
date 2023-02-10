6ad6cf3680d662fc00d82c739058e146c3f384f2
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-02-10 20:16:46 +0100

    fill card and pick

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   newCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
+  }
+
+
+  newGame() {
+    this.game = new Game();
+  }
+
   takeCard() {
+    if(this.game.stack.length){
+      const card = this.game.stack.pop();
+      if(card) {
+        this.currentCard = card;
+      }
+    }
+    
     this.newCardAnimation = true;
   }
 }
