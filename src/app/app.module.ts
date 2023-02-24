dd4b607e47bab2457cb73f55f715c7229e44e6cf
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-02-24 16:59:05 +0100

    installing firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { GameInfoComponent } from './game-info/game-info.component';
 import {MatCardModule} from '@angular/material/card';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,9 @@ import {MatCardModule} from '@angular/material/card';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
-    MatCardModule
+    MatCardModule,
+    AngularFireModule.initializeApp(environment.firebase)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyD7PjuoBdHBI2jOqNHp5Z4AvUPap-htYps",
+        authDomain: "ring-of-fire-c2fbe.firebaseapp.com",
+        projectId: "ring-of-fire-c2fbe",
+        storageBucket: "ring-of-fire-c2fbe.appspot.com",
+        messagingSenderId: "668618988738",
+        appId: "1:668618988738:web:f814a135155785f824ef5b",
+        measurementId: "G-34YD3BY6NE"
+    }
+};
